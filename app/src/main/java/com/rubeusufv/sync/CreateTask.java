b718adab5c532bee5dfddda36e6f66b4ae009c81
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2025-05-07 13:25:09 -0300

    Adição de calendário e horário na createTask

--- app/src/main/java/com/rubeusufv/sync/CreateTask.java
+++ app/src/main/java/com/rubeusufv/sync/CreateTask.java
@@ -1,32 +1,74 @@
 package com.rubeusufv.sync;
 
-import android.app.Activity;
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class CreateTask extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.timepicker.MaterialTimePicker;
+import com.google.android.material.timepicker.TimeFormat;
+
+public class CreateTask extends AppCompatActivity {
 
     // Todas as variáveis para criar uma tarefa
     private TextView nameTask;
     private EditText descriptionTask;
     private TextView dateTask;
     private TextView timeTask;
+    private MaterialButton dateButton;
+    private MaterialButton timeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
-    }
 
-    public void createTask(View v) {
         nameTask = findViewById(R.id.editTextTaskTitle);
         descriptionTask = findViewById(R.id.editTextTaskDescription);
         dateTask = findViewById(R.id.textViewDate);
         timeTask = findViewById(R.id.textViewTime);
+        dateButton = findViewById(R.id.btnDatePicker);
 
+        // Criação do calendário e exibição na tela
+        MaterialDatePicker<Long> datePicker = MaterialDatePicker
+                .Builder
+                .datePicker()
+                .setTitleText("Escolha uma data")
+                .build();
+        dateButton.setOnClickListener(v -> datePicker.show(getSupportFragmentManager(), "DATE_PICKER"));
+        datePicker.addOnPositiveButtonClickListener(selection -> {
+            dateButton.setText(datePicker.getHeaderText());
+        });
+
+
+        // Criação do horário
+        timeButton = findViewById(R.id.btnTimePicker);
+        MaterialTimePicker timePicker = new MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_24H)
+                .setHour(12)
+                .setMinute(0)
+                .setTitleText("Escolha um horário")
+                .build();
+        timeButton.setOnClickListener(v -> timePicker.show(getSupportFragmentManager(), "TIME_PICKER"));
+        timePicker.addOnPositiveButtonClickListener(v -> {
+            int hour = timePicker.getHour();
+            int minute = timePicker.getMinute();
+            @SuppressLint("DefaultLocale") String formattedTime = String.format("%02d:%02d", hour, minute);
+            timeButton.setText(formattedTime);
+        });
+    }
+
+    public void receiveNotification(View v) {
+
+    }
+
+    public void createTask(View v) {
         // Converte todas as valores em string
         String nameTaskString = nameTask.getText().toString();
         String descriptionTaskString = descriptionTask.getText().toString();
