9e25afb0d849335611b5f486f40bdc22b37fedee
joaovitormelo <joao.galo.2014@gmail.com>
2025-05-06 22:54:12 -0300

    Splash screen agora aparece no início

--- app/src/main/java/com/rubeusufv/sync/SplashActivity.java
+++ app/src/main/java/com/rubeusufv/sync/SplashActivity.java
@@ -1,21 +1,15 @@
 package com.rubeusufv.sync;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
         new Handler().postDelayed(new Runnable() {
             @Override
@@ -24,7 +18,7 @@ public class SplashActivity extends AppCompatActivity {
                 startActivity(intent);
                 finish();
             }
-            },5000);
+            },3000);
 
     }
 }
--- app/src/main/java/com/rubeusufv/sync/Tela_Cadastro.java
+++ app/src/main/java/com/rubeusufv/sync/Tela_Cadastro.java
@@ -1,5 +1,6 @@
 package com.rubeusufv.sync;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -7,18 +8,11 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-public class Tela_Cadastro extends AppCompatActivity {
+public class Tela_Cadastro extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_tela_cadastro);
         //Recuperando os id's
         EditText email=findViewById(R.id.EditEmail);
