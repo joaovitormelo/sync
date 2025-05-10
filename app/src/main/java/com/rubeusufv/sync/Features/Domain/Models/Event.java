6d1ff733c28e55494f2dd9783284cbbaf346efb7
joaovitormelo <joao.galo.2014@gmail.com>
2025-05-10 10:03:16 -0300

    Adicionados campos ao model de Event

--- app/src/main/java/com/rubeusufv/sync/Features/Domain/Models/Event.java
+++ app/src/main/java/com/rubeusufv/sync/Features/Domain/Models/Event.java
@@ -1,10 +1,37 @@
 package com.rubeusufv.sync.Features.Domain.Models;
 
-public class Event {
-    private String title;
+import com.rubeusufv.sync.Features.Domain.Types.Color;
 
-    public Event(String title) {
+import java.util.Date;
+
+public class Event {
+
+    private String title;
+    private String description;
+    private Date date;
+    private String startHour;
+    private String endHour;
+    private boolean allDay;
+    private Color color;
+    private String category;
+    private boolean rubeusSynchronized;
+    private boolean googleSynchronized;
+
+    public Event(
+        String title, String description, Date date, String startHour, String endHour,
+        boolean allDay, Color color, String category, boolean rubeusSynchronized,
+        boolean googleSynchronized
+    ) {
         this.title = title;
+        this.description = description;
+        this.date = date;
+        this.endHour = endHour;
+        this.startHour = startHour;
+        this.allDay = allDay;
+        this.color = color;
+        this.category = category;
+        this.rubeusSynchronized = rubeusSynchronized;
+        this.googleSynchronized = googleSynchronized;
     }
 
     public String getTitle() {
@@ -13,5 +40,77 @@ public class Event {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
+
+    public boolean isAllDay() {
+        return allDay;
+    }
+
+    public void setAllDay(boolean allDay) {
+        this.allDay = allDay;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isRubeusSynchronized() {
+        return rubeusSynchronized;
+    }
+
+    public void setRubeusSynchronized(boolean rubeusSynchronized) {
+        this.rubeusSynchronized = rubeusSynchronized;
+    }
+
+    public boolean isGoogleSynchronized() {
+        return googleSynchronized;
+    }
+
+    public void setGoogleSynchronized(boolean googleSynchronized) {
+        this.googleSynchronized = googleSynchronized;
     }
 }
--- app/src/main/java/com/rubeusufv/sync/Features/Domain/Repositories/EventRepository.java
+++ app/src/main/java/com/rubeusufv/sync/Features/Domain/Repositories/EventRepository.java
@@ -1,8 +1,10 @@
 package com.rubeusufv.sync.Features.Domain.Repositories;
 import com.rubeusufv.sync.Exceptions.SingletonViolationException;
 import com.rubeusufv.sync.Features.Domain.Models.Event;
+import com.rubeusufv.sync.Features.Domain.Types.Color;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 public final class EventRepository implements EventRepositoryContract {
     private static EventRepository instance;
@@ -10,8 +12,16 @@ public final class EventRepository implements EventRepositoryContract {
 
     private EventRepository() {
         eventList = new ArrayList<Event>();
-        eventList.add(new Event("Evento 1"));
-        eventList.add(new Event("Evento 2"));
+        eventList.add(new Event(
+            "Evento 1", "Um evento", new Date(2025, 5, 10),
+            "10:00", "11:00", false, Color.GREEN, "Tarefas",
+            true, true
+        ));
+        eventList.add(new Event(
+            "Evento 2", "Um outro evento", new Date(2025, 5, 8),
+            "08:00", "09:00", false, Color.BLUE, "Atividades",
+            false, true
+        ));
     }
 
     public static EventRepository createInstance(
