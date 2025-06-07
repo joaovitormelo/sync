62740f29745ddf3a109f44617876021ddbe3fdc6
joaovitormelo <joao.galo.2014@gmail.com>
2025-06-07 08:23:35 -0300

    Comentado teste

--- app/src/main/java/com/rubeusufv/sync/Features/Presentation/Screens/LoginActivity.java
+++ app/src/main/java/com/rubeusufv/sync/Features/Presentation/Screens/LoginActivity.java
@@ -51,7 +51,7 @@ public class LoginActivity extends AppCompatActivity {
         //testEditEvent();
         //testExcludeEvent();
         //testImportSingleEvent();
-        testImportEventList();
+        //testImportEventList();
     }
 
     void testImportEventList() {
