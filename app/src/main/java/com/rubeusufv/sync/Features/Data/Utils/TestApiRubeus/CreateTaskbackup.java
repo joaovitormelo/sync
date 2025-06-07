b9893d93fbd21c22b47437f7ddc98854ed655bd4
Ricardo Ferreira <141281476+ricardoof@users.noreply.github.com>
2025-06-07 11:44:17 -0300

    Conexão api da rubeus funciona

--- app/src/main/java/com/rubeusufv/sync/Features/Data/Utils/TestApiRubeus/CreateTaskbackup.java
+++ app/src/main/java/com/rubeusufv/sync/Features/Data/Utils/TestApiRubeus/CreateTaskbackup.java
@@ -1,8 +1,9 @@
-package com.rubeusufv.sync.Features.Presentation.Screens;
+package com.rubeusufv.sync.Features.Data.Utils.TestApiRubeus;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -15,14 +16,18 @@ import com.google.android.material.datepicker.MaterialDatePicker;
 import com.google.android.material.textfield.MaterialAutoCompleteTextView;
 import com.google.android.material.timepicker.MaterialTimePicker;
 import com.google.android.material.timepicker.TimeFormat;
-import com.rubeusufv.sync.Features.Data.Utils.TestApiRubeus.RubeusApiClient;
+import com.rubeusufv.sync.Features.Presentation.Screens.Task;
 import com.rubeusufv.sync.R;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-public class CreateEventActivity extends AppCompatActivity {
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class CreateTaskbackup extends AppCompatActivity {
 
     // Todas as variáveis para criar uma tarefa
     private TextView nameTask, dateTask, timeTask;
@@ -31,6 +36,9 @@ public class CreateEventActivity extends AppCompatActivity {
     private MaterialAutoCompleteTextView repeatDropdown, categoryDropdown;
     private String dateTaskString;
 
+    private static final String TAG = "RUBEUS_API_TEST";
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -109,17 +117,60 @@ public class CreateEventActivity extends AppCompatActivity {
 
 
         // Opções de categoria
-        /*categoryDropdown = findViewById(R.id.categoryDropdown);
-        String[] categoryOptions = new String[] {
-                "Reunião",
-                "Prova",
-                "Personalizar..."
-        };
-        ArrayAdapter<String> adapterCategory = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, categoryOptions);
-        categoryDropdown.setAdapter(adapterCategory);
-        categoryDropdown.setText("Selecione uma categoria", false);*/
-        categoryDropdown.setText("Selecione uma categoria", false);
+        //categoryDropdown = findViewById(R.id.categoryDropdown);
+//        String[] categoryOptions = new String[] {
+//                "Reunião",
+//                "Prova",
+//                "Personalizar..."
+//        };
+//        ArrayAdapter<String> adapterCategory = new ArrayAdapter<>(this, android.R.layout.simple_dropdown_item_1line, categoryOptions);
+//        categoryDropdown.setAdapter(adapterCategory);
+//        categoryDropdown.setText("Selecione uma categoria", false);
 
+        buscarContatoPorId("21");
+    }
+
+    public void buscarContatoPorId(String id) {
+        Log.d(TAG, "Iniciando busca por contato ID: " + id);
+        RubeusAPI apiService = RubeusApiClient.getClient().create(RubeusAPI.class);
+        ContatoRequest request = new ContatoRequest("9e5199c5de1c58f31987f71dde804da8", "7", id);
+        Call<ContatoResponse> callContato = apiService.getContatoPorId(request);
+        callContato.enqueue(new Callback<>() {
+            @Override
+            public void onResponse(Call<ContatoResponse> call, Response<ContatoResponse> response) {
+                Log.d(TAG, "Resposta recebida. Código: " + response.code());
+                if (response.isSuccessful()) {
+                    ContatoResponse contatoResponse = response.body();
+                    if (contatoResponse != null) {
+                        Log.d(TAG, "Success: " + contatoResponse.isSuccess());
+                        if (contatoResponse.getDados() != null) {
+                            ContatoDados dados = contatoResponse.getDados();
+                            Log.d(TAG, "ID: " + dados.getId());
+                            Log.d(TAG, "Nome: " + dados.getNome());
+                            Log.d(TAG, "CPF: " + dados.getCpf());
+                        } else {
+                            Log.d(TAG, "Dados é null");
+                        }
+                    } else {
+                        Log.d(TAG, "Response body é null");
+                    }
+                } else {
+                    try {
+                        String errorBody = response.errorBody() != null ?
+                                response.errorBody().string() : "Erro desconhecido";
+                        Log.e(TAG, "Erro " + response.code() + ": " + errorBody);
+                    } catch (Exception e) {
+                        Log.e(TAG, "Erro ao ler response: " + e.getMessage());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ContatoResponse> call, Throwable t) {
+                Log.e(TAG, "Falha na requisição: " + t.getMessage());
+                t.printStackTrace();
+            }
+        });
     }
 
     public void receiveNotification(View v) {
